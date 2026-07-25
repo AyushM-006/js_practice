6f4428c428eb15a992c9d6b722cac6c1cf1487b2
Ayush Mandal <mandal.ayush2006@gmail.com>
2026-07-25 23:52:44 +0530

    add Q2.js

---
+++ H2/Q2.js
@@ -0,0 +1,20 @@
+class Product {
+    name 
+    productId
+    price
+    
+    constructor(name,productId,price) {
+        this.name = name
+        this.productId = productId
+        this.price = price
+    }
+    
+    displayDetails(){
+        console.log(this.name+ " " + this.productId + " " + this.price);
+        
+    }
+
+
+}
+const p = new Product("Maggie",4412,15)
+p.displayDetails() 
