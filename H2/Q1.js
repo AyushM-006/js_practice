867a78cd20de24fcebcf2c9978c8a4e6d5acf6ac
Ayush Mandal <mandal.ayush2006@gmail.com>
2026-07-25 23:52:24 +0530

    add Q1.js

---
+++ H2/Q1.js
@@ -0,0 +1,17 @@
+const products = ["Potato" , "Onion", "Tomato","Brinjal"]
+console.log(products[3]);
+
+const[Potato,Onion,Tomato,Brinjal] = products
+
+console.log(Potato);
+
+const product = {
+  name: "Onion",
+  price: 30,
+};
+
+console.log(product.name);
+const{name, price} = product 
+
+console.log(name +" : "+ price);
+
