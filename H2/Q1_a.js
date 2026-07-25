f494776289c5ad405308eb3d6584a4bb5c2b61f1
Ayush Mandal <mandal.ayush2006@gmail.com>
2026-07-25 23:57:21 +0530

    update the object.js

--- H2/Q1_a.js
+++ H2/Q1_a.js
@@ -4,12 +4,15 @@ const arr = [1, 2, 3, 4, 5];
 const [a, b, c] = arr;
 console.log(a, b, c);
 
-//object destructuring
+
+
+//auto generated generic object
 /*const obj = { name: "John", age: 30, city: "New York" };
 const { name, age, city } = obj;
 console.log(name, age, city);
 */
 
+//object destructuring
 const products = ["Maggie","Nescafe","Coconut Oil","Sugar"]
 const [Maggie,Nescafe] = products
 
