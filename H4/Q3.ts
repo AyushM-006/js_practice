1daad66047b2bbdcab2eccdc19b8d5763f94830b
Ayush Mandal <mandal.ayush2006@gmail.com>
2026-07-31 23:14:27 +0530

    add  Q3.ts file

---
+++ H4/Q3.ts
@@ -0,0 +1,11 @@
+class Course {
+  courseName: String;
+  courseCode: Number;
+  instructor: String;
+
+  constructor(courseName: String, courseCode: Number, instructor: String) {
+    this.courseName = courseName;
+    this.courseCode = courseCode;
+    this.instructor = instructor;
+  }
+}
