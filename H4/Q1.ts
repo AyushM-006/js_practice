683586e05647677bfb9e3f63ce7078344836f804
Ayush Mandal <mandal.ayush2006@gmail.com>
2026-07-31 23:13:07 +0530

    add  Q1.ts file

---
+++ H4/Q1.ts
@@ -0,0 +1,23 @@
+class Student {
+    name:String
+    studentId:Number
+    grade:String
+    address:String
+    
+    constructor(name:String,studentId:Number,grade:String,address:String) {
+        this.name = name
+        this.studentId = studentId
+        this.grade = grade
+        this.address = address
+    }
+
+    displayInfo():void{
+        console.log(this.name );
+        console.log(this.studentId );
+        console.log(this.grade );
+        console.log(this.address );
+    }
+}
+
+const s1 = new Student("Ayush",4512,"A","Mumbai")
+s1.displayInfo()
